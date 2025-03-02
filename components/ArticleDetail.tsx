e6d0f5443b14e806ac94af98029326222151b64f
Joyce Zou <jzou771@gmail.com>
2025-03-01 21:43:04 -0500

    web view

--- components/ArticleDetail.tsx
+++ components/ArticleDetail.tsx
@@ -2,9 +2,10 @@
 import { useRoute, useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Text, View, Image, SafeAreaView, Linking, Button } from 'react-native';
+import { WebView } from 'react-native-webview';
 import { titleCase } from 'title-case';
 
-import { Article } from '../types';
+import { Article } from '../src/types';
 
 export default function ArticleDetail() {
   const route = useRoute();
@@ -12,34 +13,15 @@ export default function ArticleDetail() {
   const { article } = route.params as { article: Article };
 
   return (
-    <SafeAreaView className="my-1 flex flex-col rounded-md bg-white">
+    <SafeAreaView className="flex-1">
       <View className="self-start px-3">
         <Button title="⏴ feed" onPress={() => navigation.goBack()} />
       </View>
-      {article.img && (
-        <Image
-          className="self-center"
-          source={{ uri: article.img }}
-          style={{ width: '100%', aspectRatio: 1.5 }}
-          resizeMode="contain"
-        />
-      )}
-      <View className="mx-2 my-1 flex flex-col p-3">
-        <Text className="pb-3 text-xl font-bold">{article.title}</Text>
-        <Text className="pb-3">{article.description}</Text>
-        {/* <Text>{article.tags.join(', ')}</Text> */}
-        <Text className="font-bold">{titleCase(article.source)}</Text>
-        <Text
-          style={{ color: 'blue' }}
-          onPress={() => {
-            if (article.link) {
-              Linking.openURL(article.link);
-            }
-          }}>
-          {article.link}
-        </Text>
-        {/* <Text>{article.publishedDate.toDateString()}</Text> */}
-      </View>
+      <WebView
+        source={{ uri: article.link }}
+        style={{ flex: 1 }}
+        startInLoadingState
+      />
     </SafeAreaView>
   );
 }
